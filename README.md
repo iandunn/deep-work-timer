2fc87761ce7b0ac31043acd10a86330281acab22
Ian Dunn <ian@iandunn.name>
2020-12-01 07:46:31 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deep Work Timer
+
+This is a rough sketch of a tool that'd help me be more intentional about focusing on deep work for long periods of time, without interruptions.
---
+++ timer.js
@@ -0,0 +1,45 @@
+/*
+ * todo
+ *
+ * set active class on the body, remove
+ * popup confirm() modal if deep work < 1 hour, or something
+ */
+
+document.addEventListener( 'DOMContentLoaded', () => app.init() );
+
+const app = {
+	startTime   : 0,
+	stopwatchID : null,
+	activeTimer : null,
+
+	init: () => {
+		const buttons = document.querySelectorAll( 'button.start-timer' );
+
+		buttons.forEach( element => {
+			element.addEventListener( 'click', app.toggleTimers );
+		} );
+	},
+
+	toggleTimers: ( event ) => {
+		const timers = document.querySelectorAll( '.timer-value' );
+
+		clearInterval( app.stopwatchID );
+
+		for ( timer of timers ) {
+			if ( timer.parentElement.id === event.target.parentElement.id ) {
+				app.startTime   = Date.now();
+				app.stopwatchID = setInterval( app.updateTimer, 1000 );
+				app.activeTimer = timer;
+
+			} else {
+				timer.innerHTML = '00:00:00';
+			}
+		}
+	},
+
+	updateTimer: () => {
+		const elapsedSeconds = Date.now() - app.startTime;
+
+		app.activeTimer.innerHTML = new Date( elapsedSeconds ).toISOString().substr( 11, 8 );
+	}
+}
