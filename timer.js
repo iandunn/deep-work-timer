2d6cb0c2863341700d12390580a4ec471b19a139
Ian Dunn <ian@iandunn.name>
2020-12-06 14:59:42 -0800

    Toggle background color to make current timer obvious

--- timer.js
+++ timer.js
@@ -23,17 +23,26 @@ const app = {
 
 	// start one timer, stop the other
 	toggleTimers: ( event ) => {
-		const timers = document.querySelectorAll( '.elapsed-time' );
+		const timers     = document.querySelectorAll( '.elapsed-time' );
+		const newTimerId = event.target.dataset.timerId;
+
 		clearInterval( app.stopwatchID );
 
 		for ( timer of timers ) {
-			if ( timer.dataset.timerId === event.target.dataset.timerId ) {
+			if ( timer.dataset.timerId === newTimerId ) {
 				app.startTime   = Date.now();
 				app.stopwatchID = setInterval( app.updateTimer, 500 );
 				app.activeTimer = timer;
 
 				// This helps reinforce which mode you're in, without having to switch the active tab.
 				document.title = timer.parentElement.querySelector( 'h2' ).innerText + ' - Deep Work Timer';
+
+				/*
+				 * This makes it easy to tell which mode you're in, without having to look at each timer.
+				 *
+				 * This doesn't account for `body` having any other classes, but right now it doesn't need to.
+				 */
+				document.getElementsByTagName( 'body' )[0].classList = 'active-' + newTimerId;
 
 			} else {
 				timer.innerHTML = '00:00:00';
