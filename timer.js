2828d744e2c953853753fb5c17d35ae2e63b4069
Ian Dunn <ian@iandunn.name>
2021-02-11 10:13:22 -0800

    Convert to ES6 module

--- timer.js
+++ timer.js
@@ -3,57 +3,56 @@
  *
  * set active class on the body, remove
  * popup confirm() modal if deep work < 1 hour, or something
+ * setup HMR as progressive enhancement, don't require `watch` task to be running for normal changes though
  */
 
-const app = {
-	startTime   : 0,
-	stopwatchID : null,
-	activeTimer : null,
+let startTime = 0;
+let stopwatchID = null;
+let activeTimer = null;
 
-	// register event handlers, etc
-	init: () => {
-		const startButtons  = document.querySelectorAll( 'button.start-timer' );
+// register event handlers, etc
+const init = () => {
+	const startButtons = document.querySelectorAll( 'button.start-timer' );
 
-		startButtons.forEach( button => {
-			button.addEventListener( 'click', app.toggleTimers );
-		} );
-	},
-
-	// start one timer, stop the other
-	toggleTimers: ( event ) => {
-		const timers     = document.querySelectorAll( '.elapsed-time' );
-		const newTimerId = event.target.dataset.timerId;
-
-		clearInterval( app.stopwatchID );
-
-		for ( timer of timers ) {
-			if ( timer.dataset.timerId === newTimerId ) {
-				app.startTime   = Date.now();
-				app.stopwatchID = setInterval( app.updateTimer, 500 );
-				app.activeTimer = timer;
-
-				// This helps reinforce which mode you're in, without having to switch the active tab.
-				document.title = timer.parentElement.querySelector( 'h2' ).innerText + ' - Deep Work Timer';
-
-				/*
-				 * This makes it easy to tell which mode you're in, without having to look at each timer.
-				 *
-				 * This doesn't account for `body` having any other classes, but right now it doesn't need to.
-				 */
-				document.getElementsByTagName( 'body' )[0].classList = 'active-' + newTimerId;
-
-			} else {
-				timer.innerHTML = '00:00:00';
-			}
-		}
-	},
-
-	// print the elapsed time. callback for `setInterval()`
-	updateTimer: () => {
-		const elapsedSeconds = Date.now() - app.startTime;
-
-		app.activeTimer.innerHTML = new Date( elapsedSeconds ).toISOString().substr( 11, 8 );
-	},
+	startButtons.forEach( button => {
+		button.addEventListener( 'click', toggleTimers );
+	} );
 }
 
-app.init();
+// start one timer, stop the other
+const toggleTimers = ( event ) => {
+	const timers     = document.querySelectorAll( '.elapsed-time' );
+	const newTimerId = event.target.dataset.timerId;
+
+	clearInterval( stopwatchID );
+
+	for ( const timer of timers ) {
+		if ( timer.dataset.timerId === newTimerId ) {
+			startTime   = Date.now();
+			stopwatchID = setInterval( updateTimer, 500 );
+			activeTimer = timer;
+
+			// This helps reinforce which mode you're in, without having to switch the active tab.
+			document.title = timer.parentElement.querySelector( 'h2' ).innerText + ' - Deep Work Timer';
+
+			/*
+			 * This makes it easy to tell which mode you're in, without having to look at each timer.
+			 *
+			 * This doesn't account for `body` having any other classes, but right now it doesn't need to.
+			 */
+			document.getElementsByTagName( 'body' )[0].classList = 'active-' + newTimerId;
+
+		} else {
+			timer.innerHTML = '00:00:00';
+		}
+	}
+}
+
+// print the elapsed time. callback for `setInterval()`
+const updateTimer = () => {
+	const elapsedSeconds = Date.now() - startTime;
+
+	activeTimer.innerHTML = new Date( elapsedSeconds ).toISOString().substr( 11, 8 );
+}
+
+init();
